a522a695eae848c8b852a5895eae19b8287ff993
DaianeBS <97747870+Daianebs@users.noreply.github.com>
2023-08-26 19:50:31 +0000

    docs: readme adicionado e pequenas alteraçoes

---
+++ README.md
@@ -0,0 +1,45 @@
+# Exercício de Uso de APIs com JavaScript
+
+Este é um exercício simples para praticar o uso de APIs em JavaScript. O exercício envolve a criação de um aplicativo que gera perfis de usuário aleatórios e exibe informações como  imagem de avatar, nome, e-mail e endereço.
+
+## Como Funciona
+
+O aplicativo utiliza duas APIs diferentes para obter os dados necessários:
+
+1. [DiceBear Avatars API](https://avatars.dicebear.com/api/adventurer/): Essa API é usada para gerar imagens de avatar aleatórias com base em um "seed" específico.
+
+2. [RandomUser API](https://randomuser.me/api/): Essa API é usada para obter dados de perfis de usuário aleatórios, incluindo informações como nome, e-mail, gênero e endereço.
+
+O aplicativo é construído usando JavaScript e manipulação do DOM para atualizar a interface do usuário com os dados obtidos das APIs. Além disso, também inclui um botão que permite gerar um novo perfil de usuário aleatório a qualquer momento.
+
+## Como Executar
+
+1. Clone este repositório para o seu ambiente local.
+
+2. Abra o arquivo `index.html` em um navegador da web.
+
+3. O aplicativo será carregado e você verá as informações do perfil de usuário, incluindo o nome, e-mail, imagem de avatar e endereço.
+
+4. Clique no botão "Novo Perfil" para gerar um novo perfil de usuário aleatório a qualquer momento.
+
+## Personalizações e Melhorias
+
+O exercício é uma base sólida para a exploração de APIs em JavaScript. Você pode considerar as seguintes personalizações e melhorias:
+
+- Adicionar mais informações ao perfil de usuário, como idade, telefone, etc.
+- Melhorar a aparência da interface do usuário usando CSS.
+- Lidar de maneira mais robusta com erros na obtenção dos dados das APIs.
+- Dividir funções maiores em funções menores para tornar o código mais modular.
+- Adicionar comentários ao código para explicar a lógica por trás das diferentes partes do aplicativo.
+
+## Contribuições
+
+Contribuições são bem-vindas! Se você tiver ideias para melhorias, correções de bugs ou novos recursos, fique à vontade para abrir uma issue ou enviar um pull request.
+
+## Autor
+
+DaianeBS
+
+## Licença
+
+Este projeto está licenciado sob a [Licença MIT](https://opensource.org/licenses/MIT).
--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const API_AVATAR_URL = 'https://avatars.dicebear.com/api/adventurer/';
-const API_RANDOMUSER_URL = 'https://randomuser.me/api/';
+const API_AVATAR_URL = "https://avatars.dicebear.com/api/adventurer/";
+const API_RANDOMUSER_URL = "https://randomuser.me/api/";
 
 const elements = {
-  avatarImage: document.querySelector('#avatar-image'),
-  name: document.querySelector('#name'),
-  email: document.querySelector('#email'),
-  newProfileButton: document.querySelector('#new-profile-button'),
-  body: document.querySelector('body'),
-  username: document.querySelector('#gender'),
-  address: document.querySelector('#address'),
+  avatarImage: document.querySelector("#avatar-image"),
+  name: document.querySelector("#name"),
+  email: document.querySelector("#email"),
+  newProfileButton: document.querySelector("#new-profile-button"),
+  body: document.querySelector("body"),
+  address: document.querySelector("#address"),
 };
 
 function getRandomSeed() {
@@ -25,44 +24,38 @@ async function getRandomUser() {
     const { results } = await response.json();
     return results[0];
   } catch (error) {
-    console.error('Erro ao obter dados do usuário:', error);
+    console.error("Erro ao obter dados do usuário:", error);
     throw error;
   }
 }
 
 function updateUserInterface(user) {
-  const { name, email, gender } = user;
-  elements.name.textContent = name.first + ' ' + name.last;
+  const { name, email, location } = user;
+  elements.name.textContent = `${name.first} ${name.last}`
   elements.email.textContent = email;
-  elements.username.textContent = gender;
   elements.avatarImage.src = buildAvatarUrl(getRandomSeed());
   elements.address.textContent =
-    user.location.street.name +
-    ', ' +
-    user.location.street.number +
-    ', ' +
-    user.location.city;
+  `${location.street.name}, ${location.street.number}, ${location.city}`
   changeBackgroundColor();
 }
-//atualizar: se for masculino colocar avatar masculino consultar documentação.
-//e poderia traduzir os generos gerados
+
 async function updateProfile() {
   try {
     const user = await getRandomUser();
     updateUserInterface(user);
   } catch (error) {
-    console.error('Erro ao atualizar perfil:', error);
+    console.error("Erro ao atualizar perfil:", error);
   }
 }
 
 function changeBackgroundColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
+  const letters = "0123456789ABCDEF";
+  let color = "#";
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
-  document.querySelector('#avatar-image').style.backgroundColor = color;
+  elements.avatarImage.style.backgroundColor = color;
 }
 
-elements.newProfileButton.addEventListener('click', updateProfile);
+elements.newProfileButton.addEventListener("click", updateProfile);
 updateProfile();
